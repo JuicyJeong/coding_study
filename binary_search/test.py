4a53d9b2f9e3fdd86c889a50e30f449fe338ae2a
Joseph <jinwoo327@gmail.com>
2024-10-24 10:41:05 +0900

    이분탐색 문제 추가(1920번)

--- binary_search/test.py
+++ binary_search/test.py
@@ -1,38 +1,50 @@
+'''
+1920번
+
+N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
+
+첫째 줄에 자연수 N(1 ≤ N ≤ 100,000)이 주어진다. 
+다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 주어진다. 
+다음 줄에는 M(1 ≤ M ≤ 100,000)이 주어진다. 
+다음 줄에는 M개의 수들이 주어지는데, 이 수들이 A안에 존재하는지 알아내면 된다. 모든 정수의 범위는 -2^31 보다 크거나 같고 2^31보다 작다.
+
+M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
+
+'''
+
+##########################YOUR CODE##################################
 import sys
 
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-
-        # 원하는 값 찾은 경우 인덱스 반환
-        if array[mid] == target:
-            return mid
-        # 원하는 값이 중간점의 값보다 작은 경우 왼쪽 부분(절반의 왼쪽 부분) 확인
-        elif array[mid] > target:
-            end = mid - 1
-        # 원하는 값이 중간점의 값보다 큰 경우 오른쪽 부분(절반의 오른쪽 부분) 확인
-        else:
-            start = mid + 1
-
-    return None
-
 N = int(sys.stdin.readline())
-given_cards = list(map(int, sys.stdin.readline().split()))
-given_cards.sort()
+A = list(map(int, sys.stdin.readline().split()))
+A.sort()
 
 M = int(sys.stdin.readline())
-task_cards = list(map(int, sys.stdin.readline().split()))
-# task_cards.sort()
-
-answer = []
-for i in range(len(task_cards)):
+arr_M = list(map(int, sys.stdin.readline().split()))
 
 
-    
-    result = binary_search(given_cards, task_cards[i], 0, N - 1)
-    if result is None:
-        answer.append(0)
+
+for num_M in range(len(arr_M)):
+    found = False
+    start = 0
+    end = N-1
+
+    while start<=end:
+        mid = (start + end)//2
+        
+        if arr_M[num_M] == A[mid]:
+            found = True
+            break
+        elif arr_M[num_M] > A[mid]:
+            start = mid +1
+        else:
+            end = mid - 1
+    if not found:
+
+        # result.append(0)
+        print(0)
     else:
-        answer.append(1)
+        # result.append(1)
+        print(1)
 
-print(*answer)
+# print(*result)
